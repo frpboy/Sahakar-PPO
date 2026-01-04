8508984ef2fa590d708cbeb8cdc3c370b3b90f93
frpboy <frpboy12@gmail.com>
2026-01-04 11:08:58 +0530

    fix: temporarily disable PWA to unblock production build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,39 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
-    runtimeCaching: [
-        {
-            urlPattern: /^https:\/\/asia-south1-sahakar-ppo\.cloudfunctions\.net\/api\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-                cacheName: 'api-cache',
-                expiration: {
-                    maxEntries: 32,
-                    maxAgeSeconds: 24 * 60 * 60 // 24 hours
-                },
-                networkTimeoutSeconds: 10
-            }
-        },
-        {
-            urlPattern: /\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
-            handler: 'CacheFirst',
-            options: {
-                cacheName: 'image-cache',
-                expiration: {
-                    maxEntries: 64,
-                    maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
-                }
-            }
-        }
-    ]
-});
-
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true
+    reactStrictMode: true
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
